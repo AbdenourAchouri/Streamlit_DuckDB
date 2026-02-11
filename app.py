030f4eaec423bb2e98a949c27feb0c28467cc090
Abdenour ACHOURI <achouriabdenour.98@gmail.com>
2026-02-11 16:13:03 +0100

    Add initial Streamlit app

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Streamlit + DuckDB",
+    layout="wide"
+)
+
+st.title("📊 Application d'analyse de données")
+st.write("Bienvenue dans votre application Streamlit.")
